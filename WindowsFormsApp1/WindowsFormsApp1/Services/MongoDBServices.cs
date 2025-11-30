ce1f619ae3ebb395052ca5879e0bd2906322945a
Monzer Mahmoud <monzermahmoud90@gmail.com>
2025-11-30 18:32:58 +0200

    okk

---
+++ WindowsFormsApp1/WindowsFormsApp1/Services/MongoDBServices.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+
+namespace ClinicalBookingSystem.Services
+{
+	public class MongoDBService
+	{
+		private readonly IMongoDatabase _database;
+
+		public MongoDBService()
+		{
+			var connectionString = "mongodb://localhost:27017"; // default server
+			var client = new MongoClient(connectionString);
+
+			_database = client.GetDatabase("ClinicalBookingSystem");
+
+		}
+
+		public IMongoCollection<T> GetCollection<T>(string collectionName)
+		{
+			return _database.GetCollection<T>(collectionName);
+		}
+	}
+}
