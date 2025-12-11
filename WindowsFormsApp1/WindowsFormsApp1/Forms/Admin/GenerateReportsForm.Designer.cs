b6e50c004a2472143ddb7615ec0621f58fc9c83d
saraswelam <sara.swelam@gmail.com>
2025-12-11 02:55:03 +0200

    admin folder DONEE

--- WindowsFormsApp1/WindowsFormsApp1/Forms/Admin/GenerateReportsForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Forms/Admin/GenerateReportsForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace WindowsFormsApp1.Forms.Admin
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Forms.Admin
 {
     partial class GenerateReportsForm
     {
@@ -28,21 +31,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.MostBookedDoctorsButton = new System.Windows.Forms.Button();
+            this.AppointmentsPerClinicButton = new System.Windows.Forms.Button();
+            this.DoctorRatingsButton = new System.Windows.Forms.Button();
+            this.DoctorWorkloadButton = new System.Windows.Forms.Button();
+            this.TotalRevenueButton = new System.Windows.Forms.Button();
+            this.ReportGenerationLabel = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.MainPanel.Controls.Add(this.dataGridView);
+            this.MainPanel.Controls.Add(this.MostBookedDoctorsButton);
+            this.MainPanel.Controls.Add(this.AppointmentsPerClinicButton);
+            this.MainPanel.Controls.Add(this.DoctorRatingsButton);
+            this.MainPanel.Controls.Add(this.DoctorWorkloadButton);
+            this.MainPanel.Controls.Add(this.TotalRevenueButton);
+            this.MainPanel.Controls.Add(this.ReportGenerationLabel);
+            this.MainPanel.Location = new System.Drawing.Point(5, 5);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1208, 740);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(327, 116);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(862, 601);
+            this.dataGridView.TabIndex = 6;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // MostBookedDoctorsButton
+            // 
+            this.MostBookedDoctorsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.MostBookedDoctorsButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MostBookedDoctorsButton.Location = new System.Drawing.Point(15, 210);
+            this.MostBookedDoctorsButton.Name = "MostBookedDoctorsButton";
+            this.MostBookedDoctorsButton.Size = new System.Drawing.Size(232, 32);
+            this.MostBookedDoctorsButton.TabIndex = 5;
+            this.MostBookedDoctorsButton.Text = "Most Booked Doctors";
+            this.MostBookedDoctorsButton.UseVisualStyleBackColor = false;
+            this.MostBookedDoctorsButton.Click += new System.EventHandler(this.MostBookedDoctorsButton_Click);
+            // 
+            // AppointmentsPerClinicButton
+            // 
+            this.AppointmentsPerClinicButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.AppointmentsPerClinicButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppointmentsPerClinicButton.Location = new System.Drawing.Point(15, 301);
+            this.AppointmentsPerClinicButton.Name = "AppointmentsPerClinicButton";
+            this.AppointmentsPerClinicButton.Size = new System.Drawing.Size(232, 52);
+            this.AppointmentsPerClinicButton.TabIndex = 4;
+            this.AppointmentsPerClinicButton.Text = "Appointments Per Clinic";
+            this.AppointmentsPerClinicButton.UseVisualStyleBackColor = false;
+            this.AppointmentsPerClinicButton.Click += new System.EventHandler(this.AppointmentsPerClinicButton_Click);
+            // 
+            // DoctorRatingsButton
+            // 
+            this.DoctorRatingsButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.DoctorRatingsButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DoctorRatingsButton.Location = new System.Drawing.Point(15, 397);
+            this.DoctorRatingsButton.Name = "DoctorRatingsButton";
+            this.DoctorRatingsButton.Size = new System.Drawing.Size(232, 42);
+            this.DoctorRatingsButton.TabIndex = 3;
+            this.DoctorRatingsButton.Text = "Doctor Ratings";
+            this.DoctorRatingsButton.UseVisualStyleBackColor = false;
+            this.DoctorRatingsButton.Click += new System.EventHandler(this.DoctorRatingsButton_Click);
+            // 
+            // DoctorWorkloadButton
+            // 
+            this.DoctorWorkloadButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.DoctorWorkloadButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DoctorWorkloadButton.Location = new System.Drawing.Point(15, 494);
+            this.DoctorWorkloadButton.Name = "DoctorWorkloadButton";
+            this.DoctorWorkloadButton.Size = new System.Drawing.Size(232, 57);
+            this.DoctorWorkloadButton.TabIndex = 2;
+            this.DoctorWorkloadButton.Text = "Doctor Workload by Month";
+            this.DoctorWorkloadButton.UseVisualStyleBackColor = false;
+            this.DoctorWorkloadButton.Click += new System.EventHandler(this.DoctorWorkloadButton_Click);
+            // 
+            // TotalRevenueButton
+            // 
+            this.TotalRevenueButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.TotalRevenueButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalRevenueButton.Location = new System.Drawing.Point(15, 116);
+            this.TotalRevenueButton.Name = "TotalRevenueButton";
+            this.TotalRevenueButton.Size = new System.Drawing.Size(232, 32);
+            this.TotalRevenueButton.TabIndex = 1;
+            this.TotalRevenueButton.Text = "Total Revenue Report";
+            this.TotalRevenueButton.UseVisualStyleBackColor = false;
+            this.TotalRevenueButton.Click += new System.EventHandler(this.TotalRevenueButton_Click);
+            // 
+            // ReportGenerationLabel
+            // 
+            this.ReportGenerationLabel.AutoSize = true;
+            this.ReportGenerationLabel.BackColor = System.Drawing.Color.Violet;
+            this.ReportGenerationLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ReportGenerationLabel.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReportGenerationLabel.Location = new System.Drawing.Point(464, 34);
+            this.ReportGenerationLabel.Name = "ReportGenerationLabel";
+            this.ReportGenerationLabel.Size = new System.Drawing.Size(313, 48);
+            this.ReportGenerationLabel.TabIndex = 0;
+            this.ReportGenerationLabel.Text = "Report Generation";
             // 
             // GenerateReportsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(1243, 734);
+            this.Controls.Add(this.MainPanel);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "GenerateReportsForm";
             this.Text = "Generate Reports";
+            this.Load += new System.EventHandler(this.GenerateReportsForm_Load);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
+        private void GenerateReportsForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
         #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button MostBookedDoctorsButton;
+        private System.Windows.Forms.Button AppointmentsPerClinicButton;
+        private System.Windows.Forms.Button DoctorRatingsButton;
+        private System.Windows.Forms.Button DoctorWorkloadButton;
+        private System.Windows.Forms.Button TotalRevenueButton;
+        private System.Windows.Forms.Label ReportGenerationLabel;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/Forms/Admin/GenerateReportsForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Forms/Admin/GenerateReportsForm.cs
@@ -1,20 +1,268 @@
-﻿using System;
+﻿using ClinicalBookingSystem.Services;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1.Forms.Admin
 {
     public partial class GenerateReportsForm : Form
     {
+        private readonly MongoDBService _db;
+
         public GenerateReportsForm()
         {
             InitializeComponent();
+            _db = new MongoDBService();
+        }
+        // ================================================================
+        // Utility: Convert aggregation results into DataTable for grid
+        
+        private DataTable ConvertToTable(List<BsonDocument> docs)
+        {
+            DataTable dt = new DataTable();
+
+            if (docs.Count == 0)
+                return dt;
+
+            // Build columns
+            foreach (var element in docs[0].Elements)
+                dt.Columns.Add(element.Name);
+
+            // Add rows
+            foreach (var doc in docs)
+            {
+                DataRow row = dt.NewRow();
+                foreach (var element in doc.Elements)
+                    row[element.Name] = element.Value.ToString();
+                dt.Rows.Add(row);
+            }
+
+            return dt;
+        }
+
+        //pipeline 1
+        private void TotalRevenueButton_Click(object sender, EventArgs e)
+        {
+            var col = _db.GetCollection<BsonDocument>("payments");
+
+            var pipeline = new[]
+            {
+                new BsonDocument("$match",
+                    new BsonDocument("status", "completed")),
+
+                new BsonDocument("$addFields",
+                    new BsonDocument("paidAt",
+                        new BsonDocument("$dateFromString",
+                            new BsonDocument("dateString", "$payment_date")
+                        ))),
+
+                new BsonDocument("$group",
+                    new BsonDocument
+                    {
+                        {
+                            "_id", new BsonDocument
+                            {
+                                { "year", new BsonDocument("$year", "$paidAt") },
+                                { "month", new BsonDocument("$month", "$paidAt") }
+                            }
+                        },
+                        { "totalRevenue", new BsonDocument("$sum", "$amount") },
+                        { "count", new BsonDocument("$sum", 1) }
+                    }),
+
+                new BsonDocument("$sort",
+                    new BsonDocument
+                    {
+                        { "_id.year", -1 },
+                        { "_id.month", -1 }
+                    })
+            };
+
+            var results = col.Aggregate<BsonDocument>(pipeline).ToList();
+            dataGridView.DataSource = ConvertToTable(results);
+        }
+
+        //pipeline 2
+        private void MostBookedDoctorsButton_Click(object sender, EventArgs e)
+        {
+            var col = _db.GetCollection<BsonDocument>("appointments");
+
+            var pipeline = new[]
+            {
+                new BsonDocument("$group",
+                    new BsonDocument
+                    {
+                        { "_id", "$dr_id" },
+                        { "count", new BsonDocument("$sum", 1) }
+                    }),
+
+                new BsonDocument("$sort", new BsonDocument("count", -1)),
+                new BsonDocument("$limit", 5),
+
+                new BsonDocument("$lookup",
+                    new BsonDocument
+                    {
+                        { "from", "doctors" },
+                        { "localField", "_id" },
+                        { "foreignField", "_id" },
+                        { "as", "doctor" }
+                    }),
+
+                new BsonDocument("$unwind", "$doctor"),
+
+                new BsonDocument("$project",
+                    new BsonDocument
+                    {
+                        {
+                            "doctorName",
+                            new BsonDocument("$concat",
+                                new BsonArray { "$doctor.first_name", " ", "$doctor.last_name" })
+                        },
+                        { "count", 1 }
+                    })
+            };
+
+            var results = col.Aggregate<BsonDocument>(pipeline).ToList();
+            dataGridView.DataSource = ConvertToTable(results);
+        }
+
+        //pipeline 3
+        private void AppointmentsPerClinicButton_Click(object sender, EventArgs e)
+        {
+            var col = _db.GetCollection<BsonDocument>("appointments");
+
+            var pipeline = new[]
+            {
+                new BsonDocument("$group",
+                    new BsonDocument
+                    {
+                        { "_id", "$clinic_id" },
+                        { "totalAppointments", new BsonDocument("$sum", 1) }
+                    }),
+
+                new BsonDocument("$lookup",
+                    new BsonDocument
+                    {
+                        { "from", "clinics" },
+                        { "localField", "_id" },
+                        { "foreignField", "_id" },
+                        { "as", "clinic" }
+                    }),
+
+                new BsonDocument("$unwind", "$clinic"),
+
+                new BsonDocument("$project",
+                    new BsonDocument
+                    {
+                        { "clinicName", "$clinic.clinic_name" },
+                        { "totalAppointments", 1 }
+                    }),
+
+                new BsonDocument("$sort", new BsonDocument("totalAppointments", -1))
+            };
+
+            var results = col.Aggregate<BsonDocument>(pipeline).ToList();
+            dataGridView.DataSource = ConvertToTable(results);
+        }
+
+        //pipeline 4
+        private void DoctorRatingsButton_Click(object sender, EventArgs e)
+        {
+            var col = _db.GetCollection<BsonDocument>("reviews");
+
+            var pipeline = new[]
+            {
+                new BsonDocument("$group",
+                    new BsonDocument
+                    {
+                        { "_id", "$dr_id" },
+                        { "avgRating", new BsonDocument("$avg", "$rating") },
+                        { "count", new BsonDocument("$sum", 1) }
+                    }),
+
+                new BsonDocument("$lookup",
+                    new BsonDocument
+                    {
+                        { "from", "doctors" },
+                        { "localField", "_id" },
+                        { "foreignField", "_id" },
+                        { "as", "doctor" }
+                    }),
+
+                new BsonDocument("$unwind", "$doctor"),
+
+                new BsonDocument("$project",
+                    new BsonDocument
+                    {
+                        {
+                            "doctorName",
+                            new BsonDocument("$concat",
+                                new BsonArray { "$doctor.first_name", " ", "$doctor.last_name" })
+                        },
+                        { "avgRating", 1 },
+                        { "count", 1 }
+                    }),
+
+                new BsonDocument("$sort", new BsonDocument("avgRating", -1))
+            };
+
+            var results = col.Aggregate<BsonDocument>(pipeline).ToList();
+            dataGridView.DataSource = ConvertToTable(results);
+        }
+
+        //pipeline 5
+        private void DoctorWorkloadButton_Click(object sender, EventArgs e)
+        {
+            var col = _db.GetCollection<BsonDocument>("appointments");
+
+            var pipeline = new[]
+            {
+                new BsonDocument("$group",
+                    new BsonDocument
+                    {
+                        {
+                            "_id",
+                            new BsonDocument
+                            {
+                                { "doctorId", "$dr_id" },
+                                { "date", "$app_date" }
+                            }
+                        },
+                        { "totalAppointments", new BsonDocument("$sum", 1) }
+                    }),
+
+                new BsonDocument("$lookup",
+                    new BsonDocument
+                    {
+                        { "from", "doctors" },
+                        { "localField", "_id.doctorId" },
+                        { "foreignField", "_id" },
+                        { "as", "doctor" }
+                    }),
+
+                new BsonDocument("$unwind", "$doctor"),
+
+                new BsonDocument("$project",
+                    new BsonDocument
+                    {
+                        {
+                            "doctorName",
+                            new BsonDocument("$concat",
+                                new BsonArray { "$doctor.first_name", " ", "$doctor.last_name" })
+                        },
+                        { "date", "$_id.date" },
+                        { "totalAppointments", 1 }
+                    }),
+
+                new BsonDocument("$sort", new BsonDocument("date", -1))
+            };
+
+            var results = col.Aggregate<BsonDocument>(pipeline).ToList();
+            dataGridView.DataSource = ConvertToTable(results);
         }
     }
 }
